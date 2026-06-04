6fdfff40d1aec61504e6479b3bfce07cb96c7672
pknujra <arjunkp999@ug.cusat.ac.in>
2026-06-04 11:54:40 +0530

    Update config.h

--- Pinouts/config.h
+++ Pinouts/config.h
@@ -1,8 +1,7 @@
 #ifndef CONFIG_H
 #define CONFIG_H
-
-const uint8_t LED_STATUS = 13;
-const uint8_t BTN_MANUAL = 2;
-const uint8_t RELAY_VALVE = 8;
-
+const uint8_t RELAY_PIN = 8;
+const uint8_t BTN_PIN = 2;
+const bool RELAY_ACTIVE_LOW = true;
+const unsigned long DEBOUNCE_MS = 50;
 #endif
