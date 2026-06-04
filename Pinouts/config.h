151b05a1d97c45d98c023edb85eef979093972ce
pknujra <arjunkp999@ug.cusat.ac.in>
2026-06-04 11:10:57 +0530

    Update config.h

--- Pinouts/config.h
+++ Pinouts/config.h
@@ -1,6 +1,3 @@
-#ifndef CONFIG_H
-#define CONFIG_H
-
 // Pin Assignments
 const uint8_t LED_PIN = 13;    // Built-in LED
 const uint8_t BTN_PIN = 2;     // Push Button (Active-Low)
@@ -8,14 +5,9 @@ const uint8_t BTN_PIN = 2;     // Push Button (Active-Low)
 // Debounce Configuration
 const unsigned long DEBOUNCE_MS = 50;
 
-#endif
-
-#ifndef CONFIG_H
-#define CONFIG_H
-
 // Relay Configuration
 const uint8_t RELAY_PIN = 8;       // Relay module input pin
 const bool RELAY_ACTIVE_LOW = true; // true for active-low relay modules
 
-#endif
+
 
